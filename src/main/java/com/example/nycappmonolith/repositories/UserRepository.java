79ba5dcdc4d09555e8f50120ad49dd6f2ba6dac8
Shakaib Iqbal <shakaibiqbal@Shakaibs-MacBook-Pro.local>
2018-04-01 11:18:45 -0700

    init commit

---
+++ src/main/java/com/example/nycappmonolith/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.nycappmonolith.repositories;
+
+import com.example.nycappmonolith.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
