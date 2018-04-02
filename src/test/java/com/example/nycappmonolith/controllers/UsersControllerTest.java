16260b4d4ea78ed372d77a8dfe6c91ba2dfed495
Shakaib Iqbal <shakaibiqbal@Shakaibs-MacBook-Pro.local>
2018-04-02 13:02:22 -0700

    UserControllertest

--- src/test/java/com/example/nycappmonolith/controllers/UsersControllerTest.java
+++ src/test/java/com/example/nycappmonolith/controllers/UsersControllerTest.java
@@ -1,4 +1,27 @@
 package com.example.nycappmonolith.controllers;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UsersController.class)
 public class UsersControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void findAllUsers_success_returnsStatusOK() throws Exception {
+
+        this.mockMvc
+                .perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
 }
